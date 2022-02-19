e25b66c32eea8753660f6419411ae062e132810c
Emre INEL <mail@aemre.net>
2022-02-20 00:47:09 +0300

    Add home buttons

--- components/BigButton.tsx
+++ components/BigButton.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 
-function BigButton({ children, ...props }) {
+type BigButtonProps = {
+  children: React.ReactNode;
+  icon: string;
+  onClick: () => void;
+};
+
+function BigButton({ children, icon, onClick }: BigButtonProps) {
   return (
-    <div className="w-1/2 md:w-1/3 lg:w-1/4 h-16 bg-violet-700 p-3 rounded-r-lg relative ml-8 mt-5 flex justify-center items-center text-2xl font-sans tracking-widest cursor-pointer">
-      <div className="absolute top-0 -left-8 h-16 w-16 bg-violet-900 rounded-full border-2 border-violet-300"></div>
-      <div className="text-violet-200">Ana öğün</div>
+    <div
+      className="sm:1/2 relative ml-8 mt-5 flex h-16 w-2/3 cursor-pointer items-center justify-center rounded-r-lg bg-orange-700 p-3 font-sans text-2xl tracking-widest hover:bg-orange-600 md:w-1/3 lg:w-1/4"
+      onClick={() => onClick()}
+    >
+      <div
+        className="absolute top-0 -left-8 h-16 w-16 rounded-full border-2 border-orange-300 bg-orange-900 bg-cover"
+        style={{ backgroundImage: `url('${icon}')` }}
+      ></div>
+      <div className="text-orange-200">{children}</div>
     </div>
   );
 }
